609ce366801f60284147f7eb67b3a4e10e223b41
Rohit Singh <therohit080@gmail.com>
2019-03-17 08:54:55 +0530

    add changeColor function

--- color-game/app.js
+++ color-game/app.js
@@ -9,7 +9,7 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = colors[Math.floor(Math.random() * 6)];
 var colorDisplay = document.getElementById("colorDisplay");
 var msgDisplay = document.querySelector("#message");
 
@@ -17,18 +17,25 @@ colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
     // add initial colors to squares
-    squares[i].style.background = colors[i];
+    squares[i].style.backgroundColor = colors[i];
 
     //add click listeners to squares
     squares[i].addEventListener("click", function () {
         //grab color of clicked squares
-        var clickedColor = this.style.background;
+        var clickedColor = this.style.backgroundColor;
         //compare color to pickedColor
         if (clickedColor === pickedColor) {
             msgDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
         } else {
-            this.style.color = "#232323";
+            this.style.backgroundColor = "#232323";
             msgDisplay.textContent = "Try Again!";
         }
     });
+}
+
+function changeColors(color){
+    for(var i =0; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
 }
