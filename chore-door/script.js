041a71b1a907c7e1d3aa0e9d33699401183184d9
Rohit Singh <therohit080@gmail.com>
2019-04-08 11:11:08 +0530

    add 3 door paths

--- chore-door/script.js
+++ chore-door/script.js
@@ -1,5 +1,23 @@
 let doorImage1 = document.querySelector('#door1');
-let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+let doorImage2 = document.querySelector('#door2');
+let doorImage3 = document.querySelector('#door3');
+
+let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg" ;
+
+let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg" ;
+
+let spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg" ;
+
+
+
 doorImage1.onclick = () => {
     doorImage1.src = botDoorPath;
+};
+
+doorImage2.onclick = () => {
+    doorImage2.src = beachDoorPath;
+};
+
+doorImage3.onclick = () => {
+    doorImage3.src = spaceDoorPath;
 };
