14a3ed7f512f2a506e0a2508d0bd28dd040de838
Rohit Singh <therohit080@gmail.com>
2019-04-09 09:50:27 +0530

    add doorClicked check

--- chore-door/script.js
+++ chore-door/script.js
@@ -12,6 +12,22 @@ let numClosedDoors = 3;
 let openDoor1;
 let openDoor2;
 let openDoor3;
+let closedDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg";
+
+const isClicked = door => {
+    if (door.src === closedDoorPath) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const playDoor = () => {
+    numClosedDoors--;
+    if (numClosedDoors === 0) {
+        gameOver();
+    }
+};
 
 const randomChoreDoorGenerator = () => {
     let choreDoor = Math.floor(Math.random() * numClosedDoors);
@@ -19,27 +35,32 @@ const randomChoreDoorGenerator = () => {
         openDoor1 = botDoorPath;
         openDoor2 = beachDoorPath;
         openDoor3 = spaceDoorPath;
-
+        playDoor();
     } else if (choreDoor === 1) {
         openDoor2 = botDoorPath;
         openDoor3 = beachDoorPath;
         openDoor1 = spaceDoorPath;
-    } else {
+        playDoor();
+    } else if (choreDoor === 3) {
         openDoor3 = botDoorPath;
         openDoor1 = beachDoorPath;
         openDoor2 = spaceDoorPath;
+        playDoor();
     }
 }
 
 
 doorImage1.onclick = () => {
-    doorImage1.src = botDoorPath;
+    doorImage1.src = openDoor1;
 };
 
 doorImage2.onclick = () => {
-    doorImage2.src = beachDoorPath;
+    doorImage2.src = openDoor2;
 };
 
 doorImage3.onclick = () => {
-    doorImage3.src = spaceDoorPath;
+    doorImage3.src = openDoor3;
 };
+
+
+randomChoreDoorGenerator();
