5fa50d9be5c495abaf49eef814a8673371099e79
Rohit Singh <therohit080@gmail.com>
2019-04-09 08:52:42 +0530

    add bot appearence / random door

--- chore-door/script.js
+++ chore-door/script.js
@@ -2,12 +2,34 @@ let doorImage1 = document.querySelector('#door1');
 let doorImage2 = document.querySelector('#door2');
 let doorImage3 = document.querySelector('#door3');
 
-let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg" ;
+let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 
-let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg" ;
+let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 
-let spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg" ;
+let spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
 
+let numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+const randomChoreDoorGenerator = () => {
+    let choreDoor = Math.floor(Math.random() * numClosedDoors);
+    if (choreDoor === 0) {
+        openDoor1 = botDoorPath;
+        openDoor2 = beachDoorPath;
+        openDoor3 = spaceDoorPath;
+
+    } else if (choreDoor === 1) {
+        openDoor2 = botDoorPath;
+        openDoor3 = beachDoorPath;
+        openDoor1 = spaceDoorPath;
+    } else {
+        openDoor3 = botDoorPath;
+        openDoor1 = beachDoorPath;
+        openDoor2 = spaceDoorPath;
+    }
+}
 
 
 doorImage1.onclick = () => {
